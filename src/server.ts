4af93dd1c87100742cc9f15b1b6049fd5bdf2e6e
Elton Santos <eltonaxl@hotmail.com>
2021-04-24 08:19:14 -0300

    Finish I think

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,7 @@ import { http } from './http';
 import './websocket/client';
 import './websocket/admin';
 
-http.listen(3333, () => console.log('Server is running on port 3333'));
+// HEROKU
+const porta = process.env.PORT || 8080;
+
+http.listen(porta, () => console.log('Server is running on port 8080'));
